0396bb84c4dd9a54e563bd8631bdf5862193fe45
Himal-Thapa1 <himalthapa2020@outlook.com>
2023-08-26 12:42:54 +0545

    added one button and cal_button reusable widget

---
+++ lib/reusable_Widgets/calc_button.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+Widget calcbutton(String btntxt, Color btncolor, Color txtcolor) {
+  return Container(
+    child: ElevatedButton(
+      onPressed: () {
+        //later adding the function
+      },
+      child: Text(
+        btntxt,
+        style: TextStyle(
+          fontSize: 35,
+          color: txtcolor,
+        ),
+      ),
+      style: ButtonStyle(
+        // backgroundColor: btncolor,
+        backgroundColor: MaterialStatePropertyAll<Color>(btncolor),
+        shape: MaterialStatePropertyAll(CircleBorder()),
+        padding: MaterialStatePropertyAll(EdgeInsets.all(20)),
+      ),
+    ),
+  );
+}
--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,7 +1,9 @@
 // ignore_for_file: camel_case_types
 
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:project_abacus/constants/color.dart';
+import 'package:project_abacus/reusable_Widgets/calc_button.dart';
 
 class homeScreen extends StatefulWidget {
   const homeScreen({super.key});
@@ -13,17 +15,54 @@ class homeScreen extends StatefulWidget {
 class _homeScreenState extends State<homeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: black,
-      appBar: AppBar(
-        title: Text(
-          "ABACUS",
-          style: TextStyle(
-            color: white,
-            fontSize: 20,
+    return Material(
+      child: Scaffold(
+        backgroundColor: black,
+        appBar: AppBar(
+          title: Text(
+            "ABACUS",
+            style: GoogleFonts.itim(
+              textStyle: TextStyle(
+                color: white,
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          backgroundColor: black,
+        ),
+        body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 5),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              //
+              SingleChildScrollView(
+                scrollDirection: Axis.vertical,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Padding(padding: EdgeInsets.all(10.0)),
+                    Text(
+                      "0",
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                        color: white,
+                        fontSize: 90,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  calcbutton('AC', Colors.grey, black),
+                ],
+              )
+            ],
           ),
         ),
-        backgroundColor: black,
       ),
     );
   }
